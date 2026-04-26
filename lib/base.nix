867d9bda8171929603ad1057a7d3f28647d398c7
Sammy Scott <sammyrules100@gmail.com>
2026-04-25 18:18:54 -0600

    Fix Longhorn environment on NixOS: add bind paths for iscsid, enable rpcbind, add required packages

--- lib/base.nix
+++ lib/base.nix
@@ -27,11 +27,10 @@
       name = "iqn.2016-04.com.open-iscsi:${config.networking.hostName}";
     };
 
-    # Workaround for Longhorn on NixOS: bind Nix paths to /bin for nsenter
-    systemd.services.iscsid.serviceConfig = {
-      PrivateMounts = "yes";
-      BindPaths = "/run/current-system/sw/bin:/bin";
-    };
+    # Workaround for Longhorn on NixOS: symlink /usr/local/bin to Nix bin
+    systemd.tmpfiles.rules = [
+      "L+ /usr/local/bin - - - - /run/current-system/sw/bin/"
+    ];
 
     # Enable NFS client for Longhorn RWX volumes
     services.rpcbind.enable = true;
