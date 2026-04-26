b3f0fb2de9d6b207bd4e80a4447393bfc911e5c3
Sammy Scott <sammyrules100@gmaill.com>
2026-04-26 11:40:59 -0600

    Added longhorn

--- lib/base.nix
+++ lib/base.nix
@@ -1,17 +1,18 @@
 { pkgs, self, config, lib, ... }:
 {
    imports = [
-     ./deployment.nix
-     ./user-auth.nix
-     ./net-conf.nix
-     ./boot-conf.nix
-     ./ssh.nix
-     ./sys-conf.nix
-     ./nix-gc.nix
-     ./nix.nix
-     ./tailscale.nix
-     ./k3s.nix
-   ];
+      ./deployment.nix
+      ./user-auth.nix
+      ./net-conf.nix
+      ./boot-conf.nix
+      ./ssh.nix
+      ./sys-conf.nix
+      ./nix-gc.nix
+      ./nix.nix
+      ./tailscale.nix
+      ./kubernetes/k3s.nix
+      ./kubernetes/longhorn.nix
+     ];
 
   environment.systemPackages = [
     pkgs.ghostty.terminfo
