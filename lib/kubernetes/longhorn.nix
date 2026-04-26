dca995da1011849e79e2236eff1a25aadcb6d476
Sammy Scott <sammyrules100@gmaill.com>
2026-04-26 15:41:27 -0600

    Added longhorn

---
+++ lib/kubernetes/longhorn.nix
@@ -0,0 +1,32 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Longhorn modules
+  services.envfs.enable = true;
+  boot.kernelModules = [
+    "dm_snapshot"
+    "dm_mirror"
+    "dm_thin_pool"
+    "iscsi_tcp"
+  ];
+
+  services.openiscsi = {
+    enable = true;
+    name = "iqn.2016-04.com.open-iscsi:${config.networking.hostName}";
+  };
+
+  # Workaround for Longhorn on NixOS: symlink /usr/local/bin to Nix bin
+  systemd.tmpfiles.rules = [
+    "L+ /usr/local/bin - - - - /run/current-system/sw/bin/"
+  ];
+
+  # Enable NFS client for Longhorn RWX volumes
+  services.rpcbind.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    bash
+    nfs-utils
+    jq
+    openiscsi
+  ];
+}
