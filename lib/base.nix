4d97789797ba74fd717e1807eae2341ca00fbb8a
Sammy Scott <sammyrules100@gmail.com>
2026-04-25 18:27:36 -0600

    Fix Longhorn environment on NixOS: add bind paths for iscsid, enable rpcbind, add required packages

--- lib/base.nix
+++ lib/base.nix
@@ -1,46 +1,22 @@
 { pkgs, self, config, lib, ... }:
 {
    imports = [
-     ./deployment.nix
-     ./user-auth.nix
-     ./net-conf.nix
-     ./boot-conf.nix
-     ./ssh.nix
-     ./sys-conf.nix
-     ./nix-gc.nix
-     ./nix.nix
-     ./tailscale.nix
-     ./k3s.nix
-    ];
+      ./deployment.nix
+      ./user-auth.nix
+      ./net-conf.nix
+      ./boot-conf.nix
+      ./ssh.nix
+      ./sys-conf.nix
+      ./nix-gc.nix
+      ./nix.nix
+      ./tailscale.nix
+      ./kubernetes/k3s.nix
+      ./kubernetes/longhorn.nix
+     ];
 
-    # Longhorn modules
-    services.envfs.enable = true;
-    boot.kernelModules = [
-      "dm_snapshot"
-      "dm_mirror"
-      "dm_thin_pool"
-      "iscsi_tcp"
-    ];
-
-    services.openiscsi = {
-      enable = true;
-      name = "iqn.2016-04.com.open-iscsi:${config.networking.hostName}";
-    };
-
-    # Workaround for Longhorn on NixOS: symlink /usr/local/bin to Nix bin
-    systemd.tmpfiles.rules = [
-      "L+ /usr/local/bin - - - - /run/current-system/sw/bin/"
-    ];
-
-    # Enable NFS client for Longhorn RWX volumes
-    services.rpcbind.enable = true;
-
-  environment.systemPackages = [
-    pkgs.ghostty.terminfo
-    pkgs.bash
-    pkgs.nfs-utils
-    pkgs.jq
-  ];
+   environment.systemPackages = [
+     pkgs.ghostty.terminfo
+   ];
 
    sops = {
      defaultSopsFile = "${self}/secrets.yaml";
