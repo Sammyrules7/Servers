b527cd18699ccaa1e0b9a0d4ed0baa70ba20f453
Sammy Scott <sammyrules100@gmail.com>
2026-09-12 17:29:46 -0600

    Configure Attic for nix-snapshotter images

--- lib/nix.nix
+++ lib/nix.nix
@@ -1,6 +1,8 @@
 {lib, ...}: {
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
+    substituters = ["https://attic.maio-tech.com/main"];
+    trusted-public-keys = ["main:arW6XEJpG5vVm3SeAKZ4gohKH6xAKRN2E02iz6vgbXE="];
     max-jobs = "auto";
     cores = 0;
     build-use-substitutes = true;
